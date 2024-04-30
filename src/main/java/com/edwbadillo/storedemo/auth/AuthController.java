5f24b493c14ff7bfa982a07c24b6e3babb48fb50
Edwin Badillo <edwbadillo95@gmail.com>
2024-04-30 11:45:06 -0500

    add swagger config

--- src/main/java/com/edwbadillo/storedemo/auth/AuthController.java
+++ src/main/java/com/edwbadillo/storedemo/auth/AuthController.java
@@ -2,6 +2,12 @@ package com.edwbadillo.storedemo.auth;
 
 import com.edwbadillo.storedemo.auth.dto.JWTResponse;
 import com.edwbadillo.storedemo.auth.dto.LoginRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author edwbadillo
  */
+@Tag(name = "Auth", description = "Authentication APIs")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -21,6 +28,17 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Operation(summary = "Login customer", description = "Get a JWT for a customer.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Access token issued" ,
+                    content = { @Content(schema = @Schema(implementation = JWTResponse.class), mediaType = "application/json") }),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Invalid credentials or inactive account",
+                    content = { @Content(schema = @Schema(), mediaType = "application/json") }),
+    })
     @PostMapping("/customers/login")
     public JWTResponse loginCustomer(@Valid @RequestBody LoginRequest loginRequest) {
         return authService.loginCustomer(loginRequest);
---
+++ src/main/java/com/edwbadillo/storedemo/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.edwbadillo.storedemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(
+                        new Info()
+                        .title("Store - REST API")
+                        .description("A spring boot rest api for a store.")
+                        .version("1.0")
+                );
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+}
